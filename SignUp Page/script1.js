d7b28a0a5886a384b374febbf55ec783e5d1e228
Raj Banerjee <rajbanerjee978@gmail.com>
2022-03-30 13:11:39 +0530

    Update script1.js

--- SignUp Page/script1.js
+++ SignUp Page/script1.js
@@ -2,3 +2,10 @@ let btn = document.getElementById("button");
     let body = document.getElementById("body");
     // window.addEventListener('load',displaypage);   //Window Loading syntax
     btn.addEventListener('click',displaypage);   //Page setup
+
+    function displaypage(){
+        console.log("Working")
+        let outerbox = document.createElement('div');
+        outerbox.setAttribute("id",'box1');
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
