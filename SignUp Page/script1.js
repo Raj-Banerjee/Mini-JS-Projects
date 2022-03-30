d8b942ffa9e1752720b4ba1973c8ee0636b6e443
Raj Banerjee <rajbanerjee978@gmail.com>
2022-03-30 13:10:26 +0530

    Added Script

---
+++ SignUp Page/script1.js
@@ -0,0 +1,4 @@
+let btn = document.getElementById("button");
+    let body = document.getElementById("body");
+    // window.addEventListener('load',displaypage);   //Window Loading syntax
+    btn.addEventListener('click',displaypage);   //Page setup
