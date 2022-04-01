c296e3fe262d2b87e574bf4bd10e04bc41f81cee
Raj Banerjee <rajbanerjee978@gmail.com>
2022-04-02 00:08:24 +0530

    Adding in Script

--- SignUp Page/script1.js
+++ SignUp Page/script1.js
@@ -9,3 +9,12 @@ let btn = document.getElementById("button");
         outerbox.setAttribute("id",'box1');
         let p1 = document.createElement('p');
         let p2 = document.createElement('p');
+
+        body.appendChild(outerbox);
+        outerbox.append(p1,p2);
+        // p1.textContent("Today, Sun, 26th Apr");
+        // p2.textContent("Schedule - 7AM Onwards");
+        let text1= document.createTextNode("Today, Sun, 26th Apr");
+        let text2= document.createTextNode("Schedule - 7AM Onwards");
+        p1.appendChild(text1);
+        p2.appendChild(text2);
