4152fb44e9acbeb2f45d1ac0782fda511275816d
Raj Banerjee <rajbanerjee978@gmail.com>
2022-04-29 11:38:28 +0530

    Update movieJS.js

--- Movie-Recomendation-API/movieJS.js
+++ Movie-Recomendation-API/movieJS.js
@@ -8,3 +8,22 @@ async function findmovie(){
     let data = await res.json();
     console.log(data);
 }
+
+
+function displayData(data) {
+    data.forEach(function (product){
+        let productcard = document.createElement("div");
+
+        let img = document.createElement("img");
+        img.src = product.poster;
+
+        let title = document.createElement("p");
+        title.textContent = product.title;
+        let price = document.createElement("p");
+        price.textContent = product.price;
+
+        productcard.append(img,title,price);
+        let container = document.getElementById("container");
+        container.appendChild(productcard);
+    });
+}
