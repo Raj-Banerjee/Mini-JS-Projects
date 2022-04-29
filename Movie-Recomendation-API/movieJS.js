bc8f1b2cf7065e33c56b61621363d9cad3e15000
Raj Banerjee <rajbanerjee978@gmail.com>
2022-04-29 11:21:31 +0530

    Adding functionalities of API

--- Movie-Recomendation-API/movieJS.js
+++ Movie-Recomendation-API/movieJS.js
@@ -3,5 +3,8 @@ let container = document.getElementById("container");
 let API_KEY = 'c371194';
 
 async function findmovie(){
-    
+    let movie = document.getElementById("movie").value;
+    let res = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&t=${movie}`);
+    let data = await res.json();
+    console.log(data);
 }
